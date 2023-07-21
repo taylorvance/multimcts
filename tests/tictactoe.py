e4a739ba761f9099f5129efa17dfcd2f1c1e8185
Taylor Vance <tmax8908@yahoo.com>
2023-07-20 19:58:17 -0500

    tictactoe tweaks

--- tests/tictactoe.py
+++ tests/tictactoe.py
@@ -1,44 +1,41 @@
 from multimcts import GameState
 
 class TicTacToeState(GameState):
-    def __init__(self, board=None, current_team=1):
+    def __init__(self, board=None, player=1):
         self.board = board or [[0,0,0], [0,0,0], [0,0,0]]
-        self.current_team = current_team
+        self.player = player # 1 for X, -1 for O
 
     def get_current_team(self):
-        return self.current_team
+        return self.player
 
     def get_legal_moves(self):
         return [(i,j) for i in range(3) for j in range(3) if self.board[i][j] == 0]
 
     def make_move(self, move):
-        board = [list(row) for row in self.board]
-        board[move[0]][move[1]] = self.current_team
-        return TicTacToeState(board, -self.current_team)
+        board = [list(row) for row in self.board] # Make a copy of the board.
+        board[move[0]][move[1]] = self.player
+        return TicTacToeState(board, -self.player)
 
     def is_terminal(self):
-        # Check rows and columns.
+        # Check for 3 in a row on rows and columns.
         for i in range(3):
             if abs(sum(self.board[i])) == 3 or abs(sum([row[i] for row in self.board])) == 3:
                 return True
-        # Check diagonals.
+        # Check for 3 in a row on diagonals.
         if abs(sum(self.board[i][i] for i in range(3))) == 3 or abs(sum(self.board[i][2-i] for i in range(3))) == 3:
             return True
-        # Check for draw.
+        # Check for a draw.
         if all(self.board[i][j] != 0 for i in range(3) for j in range(3)):
             return True
         return False
 
     def get_reward(self):
-        win_sum = -self.current_team * 3
-        # Check rows and columns.
+        win_sum = -self.player * 3 # "negative player" because we're finding the reward of the previous player (the one who made the terminal move)
         for i in range(3):
             if sum(self.board[i]) == win_sum or sum(row[i] for row in self.board) == win_sum:
                 return 1
-        # Check diagonals.
         if sum(self.board[i][i] for i in range(3)) == win_sum or sum(self.board[i][2-i] for i in range(3)) == win_sum:
             return 1
-        # Cat's game.
         return 0
 
     def __repr__(self):
