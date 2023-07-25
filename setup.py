e1e4d5d1449fefa3c8bfb6d92bae95ed439dfddc
Taylor Vance <tmax8908@yahoo.com>
2023-07-25 13:53:05 -0500

    Add url

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from Cython.Build import cythonize
 
 setup(
     name='multimcts',
-    version='0.3',
+    version='0.3.1',
     description='Monte Carlo Tree Search for multiple teams',
     author='Taylor Vance',
     author_email='mirrors.cities0w@icloud.com',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license='MIT',
+    url='https://github.com/taylorvance/multimcts',
     packages=find_packages(),
     ext_modules=cythonize([
         Extension('multimcts.mcts', ['multimcts/mcts.pyx']),
